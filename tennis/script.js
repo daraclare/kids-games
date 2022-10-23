8ff800f6a5a74db8698209c54ae7439d29ed19f1
daraclare <daraclare@gmail.com>
2022-10-23 12:30:03 +0100

    done tennis

--- tennis/script.js
+++ tennis/script.js
@@ -1,22 +1,32 @@
-balloon.addEventListener("click", clickIt);
-let score = 0;
-let count = 0;
+document.addEventListener("mousemove", moveBat);
+let x = 100;
+let y = 100;
+let speedX = 2;
+let speedY = 2;
+let batX = 0;
 
-function clickIt() {
-  score++;
-  scoreBox.innerHTML = "SCORE: " + score;
-  balloon.style.height = "10px";
+function moveBat(event) {
+  batX = event.clientX;
+  bat.style.left = batX + "px";
 }
 
-function moveIt() {
-  balloon.style.height = "160px";
-  balloon.style.left = Math.random() * 800 + "px";
-  balloon.style.top = Math.random() * 600 + "px";
-  count++;
-  if (count === 40) {
-    alert("game over!");
+function moveBall() {
+  x = x + speedX;
+  y = y + speedY;
+  ball.style.left = x + "px";
+  ball.style.top = y + "px";
+
+  if (x > 530) speedX = -2;
+  if (x < 0) speedX = 2;
+  if (y < 0) speedY = 2;
+  if (y > 372 && Math.abs(batX - x) < 60) {
+    speedY = -2;
+  }
+
+  if (y > 450) {
+    alert("GAME OVER!");
     clearInterval(timer);
   }
 }
 
-var timer = setInterval(moveIt, 5000);
+const timer = setInterval(moveBall, 10);
